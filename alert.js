7ab88e304ceee82cbf720a25bbd0292eb514c92b
Islam <islam.hasuev1595@mail.ru>
2022-04-17 16:54:20 +0300

    realize magazine№1

---
+++ alert.js
@@ -0,0 +1,5 @@
+function random(min, max) {
+    Math.random(min, max)
+}
+
+// https://abcnuts.ru/sukhofrukty/
---
+++ script.js
@@ -0,0 +1,6 @@
+let arr = ["HTML", "JavaScript", "CSS"];
+
+let sorted = copySorted(arr);
+
+alert( "Я джаваскрипт!" ); // CSS, HTML, JavaScript
+ // HTML, JavaScript, CSS (без изменений)
